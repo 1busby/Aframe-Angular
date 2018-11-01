2c5bbe4315b0d98e05ede4aa9e8ef21a93a2dff8
Michael Busby <michaelb@clearlyinnovative.com>
2018-11-01 13:30:56 -0400

    remove unneccessary enitities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,47 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import {  } from 'aframe';
+import { timer } from 'rxjs/observable/timer';
 
 @Component({
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('ufoEntity') ufoEntity: ElementRef
-  loaded = false
+  @ViewChild('floatBox')
+  floatBox: ElementRef;
 
-  ufoHeight = 10;
+  @ViewChild('cursor')
+  cursor: ElementRef;
+
+  @ViewChild('camera')
+  camera: ElementRef;
+
+  loaded = false;
+
+  constructor(private cd: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
     this.loaded = true;
+    const source = timer(1000, 2000);
+    const subscribe = source.subscribe(val => {
+      console.log('camera: ', this.camera.nativeElement.getAttribute('rotation'));
+      // console.log(val);
+      // this.floatBox.nativeElement.setAttribute('position', `${val} 0.5 -3`)
+    });
+
   }
 
-  mouseEntered(event) {
-    
-  }
+  mouseEntered(event) {}
 
   onError(event) {
-    debugger
+    debugger;
+  }
+
+  boxMouseDown(event) {
+    console.log('camera: ', this.camera.nativeElement.getAttribute('position'))
+    console.log('event: ', event)
   }
 }
