82908e15be8653a9277a44baad66a57e029c57db
Michael Busby <michaelb@clearlyinnovative.com>
2018-11-01 17:37:57 -0400

    Beginning of meetup #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import {  } from 'aframe';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { timer } from 'rxjs/observable/timer';
 
 @Component({
@@ -9,39 +11,10 @@ import { timer } from 'rxjs/observable/timer';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('floatBox')
-  floatBox: ElementRef;
 
-  @ViewChild('cursor')
-  cursor: ElementRef;
-
-  @ViewChild('camera')
-  camera: ElementRef;
-
-  loaded = false;
 
   constructor(private cd: ChangeDetectorRef) {
   }
 
-  ngOnInit() {
-    this.loaded = true;
-    const source = timer(1000, 2000);
-    const subscribe = source.subscribe(val => {
-      console.log('camera: ', this.camera.nativeElement.getAttribute('rotation'));
-      // console.log(val);
-      // this.floatBox.nativeElement.setAttribute('position', `${val} 0.5 -3`)
-    });
 
-  }
-
-  mouseEntered(event) {}
-
-  onError(event) {
-    debugger;
-  }
-
-  boxMouseDown(event) {
-    console.log('camera: ', this.camera.nativeElement.getAttribute('position'))
-    console.log('event: ', event)
-  }
 }
